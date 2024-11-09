31fc6db0aafc8418f5e906c602292de57ddefc5a
lucasfafa6@gmail.com-at-851479049298 <lucasfafa6@gmail.com-at-851479049298>
2024-11-09 15:48:48 -0300

    Primeira versao de API, funcionando requisicoes basicas, ainda faltam testes mais profundos e documentacao

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from app import create_app, db
+from app.models import Usuario, Quarto, Reserva
 
-app = Flask(__name__)
+app = create_app()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
+# Cria as tabelas no banco de dados
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
