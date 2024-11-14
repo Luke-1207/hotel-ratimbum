304d425ae3bc4545438e2f5016ed0fc42e253de8
lucasfafa6@gmail.com-at-851479049298 <lucasfafa6@gmail.com-at-851479049298>
2024-11-13 23:29:21 -0300

    .

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from app.models import Usuario, Quarto, Reserva
 from flask_cors import CORS
+from populate_db import populate_database
 
 app = create_app()
 CORS(app)
@@ -8,6 +9,7 @@ CORS(app)
 # Cria as tabelas no banco de dados
 with app.app_context():
     db.create_all()
+    populate_database()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- populate_db.py
+++ populate_db.py
@@ -4,27 +4,27 @@ from werkzeug.security import generate_password_hash
 
 app = create_app()
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+def populate_database():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
 
-    user = Usuario(
-        nome="Cliente Teste",
-        email="cliente@teste.com",
-        senha=generate_password_hash("senhateste"),
-        telefone="123456789",
-        cpf="12345678901",
-        endereco="Rua de Teste, 123"
-    )
-    db.session.add(user)
-
-    for i in range(1, 51):
-        quarto = Quarto(
-            tipo='INDIVIDUAL' if i % 3 == 0 else 'DUPLO' if i % 3 == 1 else 'SUITE',
-            status='DESOCUPADO'
+        user = Usuario(
+            nome="Cliente Teste",
+            email="cliente@teste.com",
+            senha=generate_password_hash("senhateste"),
+            telefone="123456789",
+            cpf="12345678901",
+            endereco="Rua de Teste, 123"
         )
-        db.session.add(quarto)
+        db.session.add(user)
 
-    db.session.commit()
+        for i in range(1, 51):
+            quarto = Quarto(
+                tipo='INDIVIDUAL' if i % 3 == 0 else 'DUPLO' if i % 3 == 1 else 'SUITE',
+                status='DESOCUPADO'
+            )
+            db.session.add(quarto)
 
-    print("Banco de dados populado com sucesso!")
+        db.session.commit()
+        print("Banco de dados populado com sucesso!")
